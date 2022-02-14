6639bca0d9725ff3529428301a6ea7a656acf69b
muratilgun <muratilgun34@gmail.com>
2022-02-14 21:13:51 +0300

    Update SamplesViewModel.cs

--- LINQSamples/ViewModelClasses/SamplesViewModel.cs
+++ LINQSamples/ViewModelClasses/SamplesViewModel.cs
@@ -1,7 +1,42 @@
-﻿namespace LINQSamples.ViewModelClasses
+﻿using System.Collections.Generic;
+using System.Linq;
+using LINQSamples.EntityClasses;
+using LINQSamples.RepositoryClasses;
+
+namespace LINQSamples.ViewModelClasses
 {
     public class SamplesViewModel
     {
-        
+        public SamplesViewModel()
+        {
+            Products = ProductRepository.GetAll();
+        }
+
+        public bool UseQuerySyntax = true;
+        public List<Product> Products { get; set; }
+        public string ResultText { get; set; }
+
+        public void GetAll()
+        {
+            List<Product> list;
+            if (UseQuerySyntax)
+            {
+                // Query Syntax
+                list = (from prod in Products select prod).ToList();
+            }
+            else
+            {
+                //Method Syntax
+                list = Products.Select(prod => prod).ToList();
+            }
+
+            ResultText = $"Total Products: {list.Count}";
+        }
+
+        public void GetSingleColumn()
+        {
+
+        }
+
     }
 }
